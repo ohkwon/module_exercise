6181fedbd6171fe1a3b8833303e3f22cc54b01d7
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-14 21:08:49 -0700

    create pair programming directory and counter_exercise file

---
+++ pair_programming/counter_exercise.rb
@@ -0,0 +1,30 @@
+# count = 0
+
+# 11.times do
+#   puts count
+#   count += 1
+# end
+
+# result = true
+
+# unless result   #until true  == !if true  == if false
+#   puts "HELLO!"
+# end
+
+# if result
+#   puts "hello!"
+# end
+
+# sam_recipe_number = 11
+# sam_recipe = "wahtever"
+
+# sally_language_number = 6
+# sally_language = "french"
+
+# if sam_recipe_number > 10 && sally_language_number > 5
+#   puts "Sam and Sally should date."
+# end
+
+# if sam_recipe == "crepe" || sally_language == "french"
+#   puts "Sam and Sally should marry."
+# end
