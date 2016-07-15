b81b26068ff51e920d34516165664f68708afaa9
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-14 18:35:29 -0700

    create module_example.rb

---
+++ module_example.rb
@@ -0,0 +1,75 @@
+module Vehicle
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+end
+
+class Car
+
+  attr_reader :direction
+
+  include Vehicle
+
+  def initialize(vehicle_hash)
+    @speed = 0
+    @direction = 'north'
+    @fuel = vehicle_hash[:fuel]
+    @make = vehicle_hash[:make]
+    @model = vehicle_hash[:model]
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+
+  include Vehicle
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+class MountainBike
+
+  include Vehicle
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def vroom
+    puts "Vroooooooooom!"
+  end
+end
+
+car = Car.new(fuel: 50, make: "Ford", model: "Mustang")
+car.honk_horn
+
+car.turn('west')
+
+puts car.direction
+
+bike = Bike.new()
+bike.ring_bell
+
+mtbike = MountainBike.new
+mtbike.vroom
